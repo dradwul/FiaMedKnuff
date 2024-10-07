a4a6c73d65f214e4f8d07e749d085ca1d6c1f44a
Ludvig Andersson <mlt.andersson@outlook.com>
2024-10-07 18:23:58 +0200

    Gameboard UI with some logic

---
+++ FiaMedKnuff/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace FiaMedKnuff
+{
+    public class Position
+    {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
+        public bool IsOccupied { get; set; }
+
+        public Position(int rowIndex, int columnIndex)
+        {
+            RowIndex = rowIndex;
+            ColumnIndex = columnIndex;
+            IsOccupied = false;
+        }
+    }
+}
