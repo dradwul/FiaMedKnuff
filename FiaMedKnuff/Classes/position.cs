6d4004f7d284497b840bf612462e60aac008a2f0
Marcus <145341135+mlundberg93@users.noreply.github.com>
2024-10-10 13:56:38 +0200

    Added documentation for the position class

--- FiaMedKnuff/Classes/position.cs
+++ FiaMedKnuff/Classes/position.cs
@@ -11,7 +11,11 @@ namespace FiaMedKnuff
         public int RowIndex { get; set; }
         public int ColumnIndex { get; set; }
         public bool IsOccupied { get; set; }
-
+        /// <summary>
+        /// Constructor for Position with 3 parameters to set the row index, column index and if the position is occupied
+        /// </summary>
+        /// <param name="rowIndex"></param>
+        /// <param name="columnIndex"></param>
         public Position(int rowIndex, int columnIndex)
         {
             RowIndex = rowIndex;
